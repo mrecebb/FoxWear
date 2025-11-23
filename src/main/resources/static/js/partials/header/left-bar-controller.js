3807d98553d5315da92cdace4776b50589a639fa
mrecebb <mreceb0111@gmail.com>
2025-11-24 00:48:46 +0400

    prepared for front-end codding

--- src/main/resources/static/js/partials/header/left-bar-controller.js
+++ src/main/resources/static/js/partials/header/left-bar-controller.js
@@ -1,13 +1,13 @@
 function openLeftBar() {
-    document.querySelector("header .left").style.left = "0";
+    document.querySelector(".menu-bar .left").style.left = "0";
 
-    document.querySelector("header > .overlay").style.opacity = "1";
-    document.querySelector("header > .overlay").style.pointerEvents = "auto";
+    document.querySelector(".menu-bar > .overlay").style.opacity = "1";
+    document.querySelector(".menu-bar > .overlay").style.pointerEvents = "auto";
 }
 
 function closeLeftBar() {
-    document.querySelector("header .left").style.left = "calc(0px - var(--left-bar-width))";
+    document.querySelector(".menu-bar .left").style.left = "calc(0px - var(--left-bar-width))";
 
-    document.querySelector("header > .overlay").style.opacity = "0";
-    document.querySelector("header > .overlay").style.pointerEvents = "none";
+    document.querySelector(".menu-bar > .overlay").style.opacity = "0";
+    document.querySelector(".menu-bar > .overlay").style.pointerEvents = "none";
 }
